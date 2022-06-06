2e3599e91b1275ac8161ef7ae92337ec8295dcb3
Karo0510 <karma0510@gmail.com>
2022-06-06 08:14:20 +0200

    zapis do bazy

--- imports/api/methods/tasksMethods.js
+++ imports/api/methods/tasksMethods.js
@@ -3,7 +3,7 @@ import {check} from 'meteor/check'
 import {TasksCollection} from '../../db/collections/TasksCollection'
 
 Meteor.methods({
-  'tasks.insert'(text, assignedUser) {
+  'tasks.insert'(text, assignedUser, arrBackLog, arrInProgress, arrTesting, arrDone) {
     check(text, String)
     if (!this.userId) {
       throw new Meteor.Error('Not authorized')
@@ -18,6 +18,10 @@ Meteor.methods({
       createdAt: new Date(),
       userId: assignedUser ? assignedUser._id : this.userId,
       author: assignedUser ? author : null,
+      BackLog: [],
+      InProgress: [],
+      Testing: [],
+      Done: []
     })
   },
 
@@ -50,5 +54,27 @@ Meteor.methods({
         complete,
       },
     })
+  },
+
+  'tasks.update'(text, id, arrBackLog, arrInProgress, arrTesting, arrDone) {
+    check(text, String)
+    if (!id) {
+      throw new Meteor.Error('Not authorized')
+    }
+
+    console.log(arrBackLog)
+
+    var newvalues = { $set: {
+      BackLog: arrBackLog, 
+      InProgress: arrInProgress,
+      Testing: arrTesting,
+      Done: arrDone
+    } };
+
+    TasksCollection.update({text: text, _id: id}, newvalues)
+
+
+    console.log(TasksCollection.find({text: text, _id: id}))
   }
+
 })
